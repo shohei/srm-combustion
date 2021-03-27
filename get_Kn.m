84d54e7d5207c40ce26f46684bc023fc43114007
Shohei Aoki <shoaok@gmail.com>
2021-03-27 14:31:04 +0300

    fix bug: recover Kn global veriable

--- get_Kn.m
+++ get_Kn.m
@@ -1,10 +1,11 @@
-function Kn = get_Kn(r,t) 
-  global A_t; global N; 
+function Kn_ans = get_Kn(r,t) 
+  global A_t; global N; global Kn;
   global D0; global d0; global L0;
   load_param;
   x = r*t;
   d = d0 + 2*x;
   L = L0 - 2*x;
   Ab = N*(1/2*pi*(D0^2-d^2)+pi*L*d);
-  Kn = Ab / A_t;
+  Kn_ans = Ab / A_t;
+  Kn = Kn_ans;
 end
